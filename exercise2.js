e3e349f019af5149f43652c3cf67bd96a7b81337
Rebakure🔭 <165783014+KevinRebakure@users.noreply.github.com>
2025-09-10 08:15:15 -0700

    reviews exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -7,6 +7,13 @@
 // }
 
 function greet(name = "Guest") {
+  // 👇 do some research and find how this is called !!
   !!name ? `Hello, ${name}!` : "Hello!";
+}
+
+// option.
+
+function greet2(name = "Guest") {
+  return name ? `Hello, ${name}!` : "Hello!";
 }
 console.log(greet("Alice"));
