650221c0c71fb26c423214bd8ff55341fde49217
Rebakure🔭 <165783014+KevinRebakure@users.noreply.github.com>
2025-09-10 08:22:12 -0700

    reviews exercise7.js

--- exercise7.js
+++ exercise7.js
@@ -5,5 +5,21 @@
 function mult(x, y) {
   return typeof x !== "number" || typeof y !== "number" ? 0 : x * y;
 }
+// option.
+function multiply(x, y) {
+  try {
+    return x * y
+  } catch {
+    return 'The operation failed!'
+  }
+}
+
+// option.
+
+function useEarlyReturns(x, y) {
+  if(typeof x !== "number" || typeof y !== "number") return 0
+
+  return x * y
+}
 
 console.log(mult(2, "0"));
